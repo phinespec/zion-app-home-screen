b713319a3e51db15d6cd09afe7791661a21f992f
Phil Trunnell <phinespecimen@gmail.com>
2020-09-29 12:41:23 -0600

    Stackview in progress

---
+++ ZionAppHomeScreen/PlanViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlanView.swift
+//  ZionAppHomeScreen
+//
+//  Created by App Dev on 9/28/20.
+//
+
+import UIKit
+
+class PlanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ZionAppHomeScreen/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ZionAppHomeScreen
+//
+//  Created by App Dev on 9/28/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
