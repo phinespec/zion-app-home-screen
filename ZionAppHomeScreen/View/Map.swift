d4cdec486082e4ed66a1e306c32968b499b4b6c3
Phil Trunnell <phinespecimen@gmail.com>
2020-10-06 00:41:22 -0600

    More progress on the tableview for the map VC

---
+++ ZionAppHomeScreen/View/Map.swift
@@ -0,0 +1,16 @@
+//
+//  Map.swift
+//  ZionAppHomeScreen
+//
+//  Created by Philip Trunnell on 10/5/20.
+//
+
+import Foundation
+
+
+struct Map {
+    
+    let id: Int
+    let title: String
+    
+}
